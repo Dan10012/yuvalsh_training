0e59b45650913e71d5b94efdeb70539a80bc853e
yuval shpiro <yuval.shpiro@gmail.com>
2020-03-29 16:58:09 +0300

    fixing up the documantion of the module

--- rtl/avalon_enforcer.sv
+++ rtl/avalon_enforcer.sv
@@ -34,12 +34,14 @@ module avalon_enforcer
 );
  
 
-
+//////////////////////////////////////////
+//// Imports /////////////////////////////
+//////////////////////////////////////////
 import enforcer_pack::*;
 
-
-
-
+//////////////////////////////////////////
+//// Typedefs ////////////////////////////
+//////////////////////////////////////////
 typedef enum logic { // setting up the sm states
 	WAIT_FOR_SOP,
 	WAIT_FOR_EOP
@@ -47,11 +49,16 @@ typedef enum logic { // setting up the sm states
 
 
 
-
+//////////////////////////////////////////
+//// Declarations ////////////////////////
+//////////////////////////////////////////
 enforcer_sm_t    		current_state; //the sm
 logic					save_data = 1'b0; // have the responsibility to save or throw the data according to the sm
 
 
+//////////////////////////////////////////
+//// Logic ///////////////////////////////
+//////////////////////////////////////////
 assign 	untrusted_msg.rdy = trusted_msg.rdy; // setting up the untrusted rdy for the sm
 
 
@@ -63,7 +70,7 @@ always_ff @(posedge clk or negedge rst) begin : state_machine_logic
 	end else begin
 	    unique case (current_state)
 				WAIT_FOR_SOP: begin //when we got a valid sop(only sop without eop) we move on
-					if (trusted_msg.rdy & untrusted_msg.valid & untrusted_msg.sop & !untrusted_msg.eop ) begin
+					if (trusted_msg.rdy & untrusted_msg.valid & untrusted_msg.sop & !untrusted_msg.eop) begin
 						current_state <= WAIT_FOR_EOP;
 					end
 				end
@@ -81,15 +88,15 @@ end
 // this process handles the outpus of the sm_signals 
 always_comb begin : state_machine_outpus_combinational_logic
 	unique if(current_state == WAIT_FOR_SOP) begin
-		packet_in_packet 		= 0;
-		packet_didnt_started 	=  !untrusted_msg.sop & untrusted_msg.valid; // when we dont got valid sop it will rise up the indication of the eror
-		trusted_msg.sop 		= untrusted_msg.sop & untrusted_msg.valid; // the ouput sop
+		packet_in_packet 		= 0; // this indication will be always down because the packet hasnt started so it cant be a packet in packet
+		packet_didnt_started 	=  !untrusted_msg.sop & untrusted_msg.valid; // when we dont got valid sop it will rise up the indication of the eror beacause a packent didnt started
+		trusted_msg.sop 		= untrusted_msg.sop & untrusted_msg.valid; // the output sop is the same as the save data and will be up just for an valid eop
 		save_data 				= untrusted_msg.sop & untrusted_msg.valid; // to make sure we  dont save the data because the packent hasent started is up
 	end
 	else if(current_state == WAIT_FOR_EOP) begin
 		packet_in_packet 		= untrusted_msg.sop & untrusted_msg.valid; // when we got a valid eop it will rise up the inidctio of the eror
-		packet_didnt_started 	= 0; 
-		trusted_msg.sop 		= 0;// the output sop 
+		packet_didnt_started 	= 0; // this indication will be always down because th packet has already started and now the moudle wit for the eop
+		trusted_msg.sop 		= 0;// the output sop will be always down because if the module get a valid up he will revert it
 		save_data 				= 1; // because the packet didnt staret indication is down' we keep the data as it is and dont throwing it awat
 	end
 end
@@ -107,7 +114,7 @@ always_comb begin : enforcer_outputs_combinational_logic
 	// the other option of the sm outpus that means to save the data as is(in cade the packet hasnt started is dwon)
 	else if(save_data == 1)begin 
 		trusted_msg.eop 	= untrusted_msg.eop;
-		trusted_msg.empty 	= untrusted_msg.empty & trusted_msg.eop;
+		trusted_msg.empty 	= untrusted_msg.empty & trusted_msg.eop; // we add the valid sop be cause an empty is valid only at the last packet
 		trusted_msg.data 	= untrusted_msg.data;
 		trusted_msg.valid 	= untrusted_msg.valid;
 	end 
