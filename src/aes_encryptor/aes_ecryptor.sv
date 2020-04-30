c56560349758793f2f326cd78556c176d593f0a6
yuval shpiro <yuval.shpiro@gmail.com>
2020-04-30 12:59:35 +0300

    finishing the combinetional parts(msg + encryption) and using the funcs from the package

--- src/aes_encryptor/aes_ecryptor.sv
+++ src/aes_encryptor/aes_ecryptor.sv
@@ -1,4 +1,4 @@
-i//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 ///
 /// Project Name: 	aes_encryptor
 ///
@@ -22,7 +22,6 @@ i//////////////////////////////////////////////////////////////////
 
 
 module aes_encryptor 
-	
 (
 	input logic clk,
 	input logic rst,
@@ -39,7 +38,7 @@ module aes_encryptor
 //////////////////////////////////////////
 //// Imports /////////////////////////////
 //////////////////////////////////////////
-import aes_module_pack::*;
+import aes_model_pack::*;
 
 //////////////////////////////////////////
 //// Typedefs ////////////////////////////
@@ -58,7 +57,7 @@ typedef enum logic { // setting up the sm states
 aes_encryptor_sm_t    									current_state; //the sm
 logic 													deliver_msg; // signals that control whether to deliver the msg on or not(depdent on whtever encrypted or not)
 int 													counter;
-logic 	[(DATA_WIDTH_IN_BYTES*$bits(byte)) - 1 : 0] 	msg_key,round_key;
+logic 	[(DATA_WIDTH_IN_BYTES*$bits(byte)) - 1 : 0] 	msg_key,round_key, expanded_key;
 logic 	[(DATA_WIDTH_IN_BYTES*$bits(byte)) - 1 : 0] 	msg_sync,round_sync, encrypted_block; 
 logic 	[(DATA_WIDTH_IN_BYTES*$bits(byte)) - 1 : 0] 	sasb,sasr,samc; // three help signals that represnt the sync after all the encrypting levels in round
 logic 	[3 : 0] [$bits(byte) - 1 : 0]					key_rcon;
@@ -80,7 +79,7 @@ always_ff @(posedge clk or negedge rst) begin : state_machine_logic
 					end
 				end
 				ENCRYPTION_PROCESS: begin // when we got the key and the sync and we are in the middle of ecrypting it
-					if (counter = 11 ) begin
+					if (counter == 11 ) begin
 						current_state <= WAIT_FOR_MSG;
 					end
 				end	
@@ -107,21 +106,28 @@ always_ff @(posedge clk ) begin : state_machine_outpus_sequintional_logic
 	end	
 	else if(current_state == ENCRYPTION_PROCESS) begin // when the moudle is in the middle of encrypting he will block any new data(msg,sync,key) and will track on the encrypting rounds.
 		counter				<= counter +1;
-		key_rcon 			<= R_constant[counter];
+		key_rcon 			<= RCON_TABLE[counter][3:0];
 		msg_in.rdy 			<= 0;
 		key_and_sync.rdy	<= 0;
 		deliver_msg 		<= 0;
 		round_sync 			<= encrypted_sync;
 		encrypted_block		<= round_sync;
 
+		// handling the spcail rounds(first round' and las round)
 		unique if (counter == 1) begin
-			round_sync <= msg_key^msg_sync;
+			round_sync <= msg_key^msg_sync; // at the "first" round just xoring the sync and key
 		end
 		else if(counter == 10)begin
-			round_sync <= round_key^lr_sync;
+			round_sync <= round_key^sasr; // in the last round avoding the mix_col
 		end
 		else
-			round_sync <= round_key^round_sync;
+			round_sync <= round_key^round_sync; // regular round
+		end
+
+		unique if (counter == 1) begin
+			round_key = msg_key;
+		else
+			round_key = expanded_key;
 		end
 	end
 	else if (current_state == WAIT_FOR_MSG) begin// when the module finish creating the encrypted block he will wait for a msg to encrypit it and then he will go back to encryption(or to the statrt if msg has ended)
@@ -132,7 +138,19 @@ always_ff @(posedge clk ) begin : state_machine_outpus_sequintional_logic
 	end	
 end
 
-// handle with tje forwarding of the msg
+
+// the encrypted_round module
+always_comb begin: encrypted_round
+	sasb = subbytes(round_sync);
+	sasr = shift_rows(sasb);
+	samc = mix_colums(sasr);
+	expanded_key = key_expand(round_key,key_rcon);
+	encrypted_sync = samc^expanded_key;
+
+
+
+
+// handle with the forwarding of the msg
 always_comb begin : sm_msg_handling
 	msg_out.data 	= msg_in.data^encrypted_block;
 	msg_out.rdy		= msg_in.rdy;
